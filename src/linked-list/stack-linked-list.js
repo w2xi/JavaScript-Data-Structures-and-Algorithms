1a01b7639b730b4dc9e3d401db1552e09ab6d74f
w2xi <43wangxi@gmail.com>
2022-05-14 12:02:31 +0800

    feat: Stack Linekd List

---
+++ src/linked-list/stack-linked-list.js
@@ -0,0 +1,42 @@
+const DoublyLinkedList = require('./doubly-linked-list')
+
+function StackLinkedList() {
+  let items = DoublyLinkedList()
+
+  return {
+    push(element) {
+      items.push(element);
+    },
+
+    pop() {
+      if (!this.isEmpty()) {
+        return items.removeAt(items.size() - 1);
+      }
+    },
+
+    peek() {
+      if (!items.isEmpty()) {
+        return items.getElementAt(items.size() - 1).val;
+      }
+    },
+
+    isEmpty() {
+      return items.isEmpty();
+    },
+
+    size() {
+      return items.size();
+    },
+
+    clear() {
+      items.clear();
+    },
+
+    toString() {
+      return items.toString();
+    }
+  }
+}
+
+module.exports = StackLinkedList;
+
