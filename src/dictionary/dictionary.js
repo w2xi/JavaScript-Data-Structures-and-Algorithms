06ec9200d9c6bacdbf77c1d0d28fa4fea814e70d
w2xi <43wangxi@gmail.com>
2022-05-16 17:00:41 +0800

    feat: Dictionary

---
+++ src/dictionary/dictionary.js
@@ -0,0 +1,124 @@
+const defaultToString = (item) => {
+  if (item === null) {
+    return 'NULL';
+  } else if (item === undefined) {
+    return 'UNDEFINED';
+  } else if (typeof item === 'string' || item instanceof String) {
+    return `${item}`;
+  }
+  return item.toString(); 
+};
+
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+  toString() {
+    return `[#${this.key}: ${this.value}]`;
+  }
+}
+
+class Dictionary {
+  constructor() {
+    this.toStrFn = defaultToString;
+    this.table = {};
+  }
+
+  set(key, value) {
+    if (key && value) {
+      this.table[this.toStrFn(key)] = new ValuePair(key, value);
+      return true;
+    }
+    return false;
+  }
+
+  get(key) {
+    const valuePair = this.table[this.toStrFn(key)];
+    return valuePair ? valuePair.value : undefined;
+  }
+
+  remove(key) {
+    if (this.hasKey(key)) {
+      delete this.table[this.toStrFn(key)];
+      return true;
+    }
+    return false;
+  }
+
+  hasKey(key) {
+    return this.table[this.toStrFn(key)] != undefined;
+  }
+
+  values() {
+    return this.keyValues().map(valuePair => valuePair.value);
+  }
+
+  keys() {
+    return this.keyValues().map(valuePair => valuePair.key);
+  }
+
+  keyValues() {
+    return Object.values(this.table);
+  }
+
+  forEach(callbackFn) {
+    const valuePairs = this.keyValues();
+    for (let i = 0; i < valuePairs.length; i++) {
+      const result = callbackFn(valuePairs[i].key, valuePairs[i].value);
+      if (result === false) {
+        break;
+      }
+    }
+  } 
+
+  size() {
+    return Object.keys(this.table).length;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  clear() {
+    this.table = {};
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const valuePairs = this.keyValues();
+    let objString = `${valuePairs[0].toString()}`;
+
+    for (let i = 1; i < valuePairs.length; i++) {
+      objString = `${objString},${valuePairs[i].toString()}`;
+    }
+    return objString;
+  }
+}
+
+const dictionary = new Dictionary();
+
+dictionary.set('Gandalf', 'gandalf@email.com')
+dictionary.set('John', 'johnsnow@email.com')
+dictionary.set('Tyrion', 'tyrion@email.com')
+
+console.log(dictionary.hasKey('Gandalf')); // true
+console.log(dictionary.size()); // 3
+
+console.log(dictionary.keys());
+console.log(dictionary.values());
+console.log(dictionary.get('Tyrion'));
+
+console.log('------------------');
+
+dictionary.remove('John');
+
+console.log(dictionary.keys());
+console.log(dictionary.values());
+console.log(dictionary.keyValues());
+
+dictionary.forEach((k, v) => {
+  console.log('forEach: ', `key: ${k}, value: ${v}`);
+});
