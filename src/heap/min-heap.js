853b33c60ad52787a0904ef73913b0ed9341968d
w2xi <43wangxi@gmail.com>
2022-05-25 22:18:00 +0800

    feat: Min Heap

--- src/heap/min-heap.js
+++ src/heap/min-heap.js
@@ -24,7 +24,7 @@ class MinHeap {
 
   insert(value) {
     if (value) {
-      // 插入到堆的底部
+      // 插入到堆的底部，即数组的最后一个位置
       this.heap.push(value);
       // 执行上移操作
       this.siftUp(this.heap.length - 1);
@@ -33,32 +33,72 @@ class MinHeap {
     return false;
   }
 
+  // 上移操作
   siftUp(index) {
+    // 父节点索引
     const parentIndex = this.getParentIndex(index);
-    if (index > 0 && this.compareFn(this.heap[index], this.heap[parentIndex]) === Compare.LESS_THAN) {
+    const currentValue = this.heap[index];
+    const parentValue = this.heap[parentIndex];
+
+    // 如果当前节点的索引大于0 且 当前节点值小于父节点值 执行上移操作
+    if (index > 0 && this.compareFn(currentValue, parentValue) === Compare.LESS_THAN) {
+      // 交换父节点和当前节点的值
       swap(this.heap, index, parentIndex);
+      // 继续上移
       this.siftUp(parentIndex);
     }
   }
-  // 移除最小值
-  extract(index = 0) {
-    const leftChildIndex = this.getLeftChildIndex(index);
-    const rightChildIndex = this.getRightChildIndex(index);
-    const lastIndex = this.size() - 1;
 
-    if (leftChildIndex <= lastIndex) {
-      this.heap[index] = this.heap[leftChildIndex];
-      this.extract(leftChildIndex);
-    } else if (rightChildIndex <= lastIndex) {
-      this.heap[index] = this.heap[rightChildIndex];
-      this.extract(rightChildIndex);
+  // 移除堆中的最小值，即根节点
+  // 在移除后，需要将堆的最后一个节点放到根节点的位置，然后再执行下移操作
+  // 直到堆的结构正常
+  extract() {
+    if (this.isEmpty()) {
+      return undefined;
     }
+    if (this.size() === 1) {
+      return this.heap.shift();
+    }
+    // 移除值
+    const removedValue = this.heap[0];
+    // 将堆的最后一个节点放到根节点的位置
+    this.heap[0] = this.heap.pop();
+    // 执行下移操作
+    this.siftDown(0);
+
+    return removedValue;
+  }
+
+  // 下移操作
+  siftDown(index) {
+    let element = index;
+    const left = this.getLeftChildIndex(index);
+    const right = this.getRightChildIndex(index);
+    const size = this.size();
+
+    // 如果当前节点值大于左子节点值，说明需要往左下移
+    if (left < size && this.compareFn(this.heap[element], this.heap[left]) === Compare.BIGGER_THAN) {
+      element = left;
+    }
+    // 如果当前节点值大于右子节点值，说明需要往右下移
+    if (right < size && this.compareFn(this.heap[element], this.heap[right]) === Compare.BIGGER_THAN) {
+      element = right;
+    }
+    if (element !== index) {
+      swap(this.heap, index, element);
+      // 继续下移
+      this.siftDown(element);
+    }    
   }
   
   findMinimum() {
     if (!this.isEmpty()) {
       return this.heap[0];
     }
+  }
+
+  getAsArray() {
+    return this.heap;
   }
 
   getLeftChildIndex(parentIndex) {
@@ -86,12 +126,18 @@ class MinHeap {
   }
 }
 
-const heap = new MinHeap();
+// const heap = new MinHeap();
 
-heap.insert(2);
-heap.insert(3);
-heap.insert(4);
-heap.insert(5);
-heap.insert(1);
+// heap.insert(2);
+// heap.insert(3);
+// heap.insert(4);
+// heap.insert(5);
+// heap.insert(1);
 
-console.log(heap.heap);
+// console.log(heap.getAsArray());   // [1, 2, 4, 5, 3]
+// console.log(heap.isEmpty());      // false
+// console.log(heap.size());         // 5
+// console.log(heap.findMinimum());  // 1
+
+// heap.extract();
+// console.log(heap.getAsArray());  // [2, 3, 4, 5]
